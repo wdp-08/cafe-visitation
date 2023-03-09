bbbf40daecedeed241027b80a190460bc4c3c6b2
Adrian Muhammad Kuncoro <57836873+dakochan666@users.noreply.github.com>
2023-03-09 13:49:58 +0700

    Create index,js

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Seseorang pengunjung bar direpresentasikan oleh JavaScript memiliki variable name, age, dan money.
+ * Ia masuk ke bar untuk memesan minuman, namun bar memiliki kondisi sebagai berikut:
+ *
+ * - Jika name dari si pengunjung kosong, tampilkan di console "Anda tidak boleh masuk!" dan program selesai.
+ *   Jika name tidak kosong, lanjut ke step ke 2.
+ *
+ * - Jika age dari si pengunjung dibawah 17 tahun, maka ia hanya boleh memesan juice.
+ *   Jika age 17 tahun keatas, ia hanya boleh memesan anggur.
+ *
+ * - Juice memiliki harga 50000, sedangkan anggur memiliki harga 300000.
+ *   Jika money/uang yang dimiliki tidak mencukupi, maka tampilkan di console "Uang tidak cukup. Anda harus pulang.".
+ *   Jika uang cukup, tampilkan "Anda bisa pesan minum. Sisa uang anda: [...]",
+ *   dan ganti [...] dengan sisa uang yang telah dikurangi oleh harga minuman.
+ */
+
+ let name = 'Ahmad'
+ let age = 17
+ let money = 2000000
